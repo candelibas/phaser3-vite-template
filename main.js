973bb3bc326956f080ead229b694bf48667e4203
Can Delibas <candelibas96@gmail.com>
2021-09-22 15:52:38 +0300

    integrate phaser

---
+++ main.js
@@ -0,0 +1,37 @@
+import "./style.css";
+
+import Phaser from "phaser";
+import logoImg from "./src/assets/logo.png";
+
+class MyGame extends Phaser.Scene {
+  constructor() {
+    super();
+  }
+
+  preload() {
+    this.load.image("logo", logoImg);
+  }
+
+  create() {
+    const logo = this.add.image(400, 150, "logo");
+
+    this.tweens.add({
+      targets: logo,
+      y: 450,
+      duration: 2000,
+      ease: "Power2",
+      yoyo: true,
+      loop: -1,
+    });
+  }
+}
+
+const config = {
+  type: Phaser.AUTO,
+  parent: "phaser-canvas",
+  window: 800,
+  height: 600,
+  scene: MyGame,
+};
+
+const game = new Phaser.Game(config);
